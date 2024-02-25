6c817ba711c306b133169e3bf3f167aab0254976
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-02-25 19:38:38 +0800

    adding page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [subscribed, setSubscribed] = useState(false);
+
+  const handleSubscribe = () => {
+    if (email.trim() === "") {
+      setSubscribed(false);
+      setError(true);
+    } else {
+      setSubscribed(true);
+      setError(false);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="bg-white lg:py-4 lg:px-4 lg:pl-14 rounded-3xl max-w-80 lg:max-w-4xl">
-        <div className="grid gap-8 lg:grid-cols-2 lg:gap-14">
+        <div
+          className={
+            subscribed ? "hidden" : `grid gap-8 lg:grid-cols-2 lg:gap-14`
+          }
+        >
           <div className="space-y-4 flex flex-col justify-center px-6 pb-10 lg:pb-0 lg:px-0 order-last">
             <p className="text-black font-bold text-4xl lg:text-6xl pb-2 lg:pb-4 tracking-tighter">
               Stay updated!
@@ -35,11 +55,24 @@ function App() {
               </label>
               <input
                 type="email"
-                className="rounded-md w-full bg-white border-2 border-gray-200 px-5 py-3 text-sm focus:border-black focus:ring-2 focus:ring-black"
+                className={`rounded-md w-full  border-2 ${
+                  error
+                    ? "bg-red-100 text-red-200 border-red-200"
+                    : "bg-white text-black border-gray-200"
+                }  px-5 py-3 text-sm focus:border-black focus:ring-2 focus:ring-black`}
                 placeholder="email@company.com"
+                onChange={(e) => setEmail(e.target.value)}
               />
+              {error && (
+                <p className="text-red-500 text-sm font-semibold">
+                  Please enter valid email
+                </p>
+              )}
             </div>
-            <button className="bg-blue-950 transition-colors duration-300 ease-in-out rounded-lg px-4 lg:px-8 py-3 font-semibold hover:bg-gradient-to-r from-rose-400 to-orange-300">
+            <button
+              onClick={handleSubscribe}
+              className="bg-blue-950 transition-colors duration-300 ease-in-out rounded-lg px-4 lg:px-8 py-3 font-semibold hover:bg-gradient-to-r from-rose-400 to-orange-300"
+            >
               Subscribe to monthly newsletter
             </button>
           </div>
@@ -55,6 +88,21 @@ function App() {
               className="block lg:hidden"
             />
           </div>
+        </div>
+
+        {/* subscribed */}
+        <div className="space-y-5 py-8 max-w-lg pr-10">
+          <img src="icon-success.svg" alt="success" />
+          <p className="text-black font-bold text-3xl lg:text-6xl">
+            Thanks for subscribing!
+          </p>
+          <p className="text-black font-semibold">
+            A confirmation email has been sent to {email}. Please open it and
+            click the button inside to confirm your subscription.
+          </p>
+          <button className="bg-blue-950 transition-colors w-full duration-300 ease-in-out rounded-lg px-4 lg:px-8 py-3 font-semibold hover:bg-gradient-to-r from-rose-400 to-orange-300">
+            Dismiss message
+          </button>
         </div>
       </div>
     </div>
